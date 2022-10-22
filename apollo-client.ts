e68bee5421e56b457ac308cf5a5556a29e4f1578
Sekai <50212311+ig4e@users.noreply.github.com>
2022-10-22 19:55:38 +0200

    Update apollo-client.ts

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-	uri: "https://apieu.emanga.tk/graphql", //"https://api.emanga.tk/graphql",
+	uri: "https://api.emanga.tk/graphql", //"https://api.emanga.tk/graphql",
 	cache: new InMemoryCache(),
 });
