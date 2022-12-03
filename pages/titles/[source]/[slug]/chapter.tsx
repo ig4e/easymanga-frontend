e413f731b75e2f561f4490daaa4e60226ea62ee6
Sekai <50212311+ig4e@users.noreply.github.com>
2022-12-03 02:37:19 +0200

    Update chapter.tsx

--- pages/titles/[source]/[slug]/chapter.tsx
+++ pages/titles/[source]/[slug]/chapter.tsx
@@ -246,53 +246,6 @@ const Chapter: NextPage<IPageProps> = ({ chapter, manga }) => {
 						}%`,
 					}}
 				>
-					<AnimatePresence>
-						{loading && (
-							<motion.div
-								initial={{ scale: 0 }}
-								animate={{ scale: 1 }}
-								exit={{ scale: 0 }}
-								className="flex justify-center py-4"
-							>
-								<div className="flex items-center gap-4">
-									{!currentChapter.nextSlug ? (
-										<>
-											<span>
-												{getRandomEmoji()} No Next
-												Chapter, so sad....
-											</span>
-										</>
-									) : (
-										<>
-											<svg
-												className="animate-spin w-12 h-12"
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-											>
-												<circle
-													className="opacity-25 text-neutral-200"
-													cx="12"
-													cy="12"
-													r="10"
-													stroke="currentColor"
-													strokeWidth="4"
-												></circle>
-												<path
-													className="opacity-75 text-primary"
-													fill="currentColor"
-													d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-												></path>
-											</svg>
-											<span>
-												{getRandomEmoji()} Loading....
-											</span>
-										</>
-									)}
-								</div>
-							</motion.div>
-						)}
-					</AnimatePresence>
 					{
 						allChapters!.map((chapter, chapterIndex) => {
 							const lastChapter = allChapters.find(
@@ -348,7 +301,7 @@ const Chapter: NextPage<IPageProps> = ({ chapter, manga }) => {
 								exit={{ scale: 0 }}
 								className="flex justify-center py-4"
 							>
-								<div className="flex items-center gap-4">
+								<div className="flex items-center gap-4 text-sm">
 									{!currentChapter.nextSlug ? (
 										<>
 											<span>
@@ -359,7 +312,7 @@ const Chapter: NextPage<IPageProps> = ({ chapter, manga }) => {
 									) : (
 										<>
 											<svg
-												className="animate-spin w-12 h-12"
+												className="animate-spin w-10 h-10"
 												xmlns="http://www.w3.org/2000/svg"
 												fill="none"
 												viewBox="0 0 24 24"
