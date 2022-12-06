b0c80053a70b731c1113f1f889f46f91ed150a3e
Sekai <ahmedx793@gmail.com>
2022-12-06 03:53:10 +0200

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
 	return (
@@ -6,15 +7,46 @@ export default function Document() {
 			<Head>
 				<meta name="theme-color" content="#FFFFFF" />
 				<link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <meta property="og:image" content="/assets/logo-1024x1024.png" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<meta
+					property="og:image"
+					content="/assets/logo-1024x1024.png"
+				/>
 			</Head>
 			<body>
 				<Main />
 				<NextScript />
 			</body>
+			<Script
+				src="https://www.googletagmanager.com/gtag/js?id=G-5B28VN7941"
+				strategy="afterInteractive"
+			/>
+
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+          window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+		
+		  gtag('config', 'G-5B28VN7941');
+        `}
+			</Script>
 		</Html>
 	);
 }
