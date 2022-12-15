42cc27395600d2aff33fbdeb31ecea3a445db4bd
Sekai <50212311+ig4e@users.noreply.github.com>
2022-12-16 01:33:55 +0200

    Update apollo-client.ts

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,13 +1,14 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const useDevServer = false;
+const useCyclicServer = true;
 
 export const client = new ApolloClient({
 	uri:
 		process.env.NODE_ENV === "development"
 			? useDevServer
-				? "http://localhost:3000/graphql"
-				: "https://fr.emanga.tk/graphql"
+				? "http://localhost:3000/graphql" 
+				: useCyclicServer ? "https://wicked-gloves-moth.cyclic.app" : "https://fr.emanga.tk/graphql"
 			: "https://fr.emanga.tk/graphql", //"https://api.emanga.tk/graphql",
 	cache: new InMemoryCache(),
 });
