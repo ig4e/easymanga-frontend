d2fc38fe09111ce4233f16c75ffd55ef9c6d526f
Sekai <ahmedx793@gmail.com>
2022-12-29 20:36:06 +0200

    Update privacy.tsx

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -147,7 +147,7 @@ function privacy() {
 					Service.
 				</p>
 				<p>
-					Usage Data may include information such as Your Device's
+					Usage Data may include information such as Your Device&apos;s
 					Internet Protocol address (e.g. IP address), browser type,
 					browser version, the pages of our Service that You visit,
 					the time and date of Your visit, the time spent on those
@@ -305,7 +305,7 @@ function privacy() {
 							<strong>To contact You:</strong> To contact You by
 							email, telephone calls, SMS, or other equivalent
 							forms of electronic communication, such as a mobile
-							application's push notifications regarding updates
+							application&apos;s push notifications regarding updates
 							or informative communications related to the
 							functionalities, products or contracted services,
 							including the security updates, when necessary or
@@ -416,7 +416,7 @@ function privacy() {
 				<h2>Transfer of Your Personal Data</h2>
 				<p>
 					Your information, including Personal Data, is processed at
-					the Company's operating offices and in any other places
+					the Company&apos;s operating offices and in any other places
 					where the parties involved in the processing are located. It
 					means that this information may be transferred to — and
 					maintained on — computers located outside of Your state,
@@ -502,7 +502,7 @@ function privacy() {
 					strive to use commercially acceptable means to protect Your
 					Personal Data, We cannot guarantee its absolute security.
 				</p>
-				<h1>Children's Privacy</h1>
+				<h1>Children&apos;s Privacy</h1>
 				<p>
 					Our Service does not address anyone under the age of 13. We
 					do not knowingly collect personally identifiable information
@@ -516,14 +516,14 @@ function privacy() {
 				<p>
 					If We need to rely on consent as a legal basis for
 					processing Your information and Your country requires
-					consent from a parent, We may require Your parent's consent
+					consent from a parent, We may require Your parent&apos;s consent
 					before We collect and use that information.
 				</p>
 				<h1>Links to Other Websites</h1>
 				<p>
 					Our Service may contain links to other websites that are not
 					operated by Us. If You click on a third party link, You will
-					be directed to that third party's site. We strongly advise
+					be directed to that third party&apos;s site. We strongly advise
 					You to review the Privacy Policy of every site You visit.
 				</p>
 				<p>
