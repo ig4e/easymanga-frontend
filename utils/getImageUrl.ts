50bf875e98598cccb7e91341752c662bc01af7a7
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2023-12-29 19:43:00 +0200

    Update getImageUrl.ts

--- utils/getImageUrl.ts
+++ utils/getImageUrl.ts
@@ -1,5 +1,5 @@
 export function getWorkersUrl(src: string, referer?: string) {
-	return `https://workers.emanga.tk/fetch?url=${src}&referer=${
+	return `https://easymangaproxy.sekai966.workers.dev/fetch?url=${src}&referer=${
 		referer ? referer : "mangadex.org"
 	}`;
 }
